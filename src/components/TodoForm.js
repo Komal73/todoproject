6c082f13006098c9e0f659af6862f6daf554399b
KOMAL <sharma3.komal@gmail.com>
2023-12-22 00:25:27 +0530

    button and usestate

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const TodoForm = () => {
+    const [value, setValue] = useState("")
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log(value)
+    }
   return (
-    <div>TodoForm</div>
+    <form className='Todoform' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' placeholder='what is the task today?' onChange={(e) =>setValue(e.target.value) }/>
+        <button type='submit' className='todo-btn'>Add Task</button>
+        
+    
+    
+    </form>
   )
 }
